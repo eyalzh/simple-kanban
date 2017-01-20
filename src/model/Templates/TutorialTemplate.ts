789d0c0589dc07dbd11814606844eec353903ee9
Eyal Z <eyalzh@gmail.com>
2017-01-20 19:27:13 +0200

    add TutorialTemplate and call it when initializing the app, add markdown example to the tutorial

---
+++ src/model/Templates/TutorialTemplate.ts
@@ -0,0 +1,30 @@
+import {Template} from "./Template";
+import TaskModel from "../TaskModel";
+
+export default class TutorialTemplate implements Template {
+
+    public getName(): string {
+        return "Tutorial";
+    }
+
+    public async applyOnModel(model: TaskModel): Promise<void> {
+
+        const backlog = await model.addColumn("Backlog", 10);
+        const next = await model.addColumn("Next", 3);
+        await model.addColumn("Done", 10);
+
+        await model.addTask(next, "Create a new task by double clicking anywhere on this column");
+        await model.addTask(backlog, "Create a new column by pressing the 'Add Column' button at the upper right corner of the page");
+        await model.addTask(backlog, "Drag this task from this column to the next column");
+        await model.addTask(backlog, "Delete this task by dragging it to the 'Trash Zone'");
+        await model.addTask(backlog, "Edit this task by double clicking on it",
+            "# Detailed Description \n\n" +
+            "You can use [Markdown ](http://https://en.wikipedia.org/wiki/Markdown) syntax in the task's description " +
+            "and see it rendered on the right.");
+        await model.addTask(backlog, "Use #hashtags in tasks' text");
+        await model.addTask(backlog, "Create a new board by pressing the 'Add Board' button at the upper right corner of the page");
+        await model.addTask(backlog, "Switch between boards by pressing Alt+B or using the select box at the upper right corner of the page");
+
+    }
+
+}
--- src/model/initializeModel.ts
+++ src/model/initializeModel.ts
@@ -1,4 +1,5 @@
 import TaskModel, {FLAGS} from "./TaskModel";
+import TutorialTemplate from "./Templates/TutorialTemplate";
 
 export default async function initializeModel(model: TaskModel) {
 
@@ -15,18 +16,8 @@ export default async function initializeModel(model: TaskModel) {
 
         if (columns.length === 0) {
 
-            const backlog = await model.addColumn("Backlog", 10);
-            const next = await model.addColumn("Next", 3);
-            await model.addColumn("Done", 10);
-
-            await model.addTask(next, "Create a new task by double clicking anywhere on this column");
-            await model.addTask(backlog, "Create a new column by pressing the 'Add Column' button at the upper right corner of the page");
-            await model.addTask(backlog, "Drag this task from this column to the next column");
-            await model.addTask(backlog, "Delete this task by dragging it to the 'Trash Zone'");
-            await model.addTask(backlog, "Edit this task by double clicking on it");
-            await model.addTask(backlog, "Use #hashtags in tasks' text");
-            await model.addTask(backlog, "Create a new board by pressing the 'Add Board' button at the upper right corner of the page");
-            await model.addTask(backlog, "Switch between boards by pressing Alt+B or using the select box at the upper right corner of the page");
+            const tutorialTemplate = new TutorialTemplate();
+            await tutorialTemplate.applyOnModel(model);
 
         }
 
