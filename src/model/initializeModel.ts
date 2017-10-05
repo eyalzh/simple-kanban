27b14674692019420db87b988eb00eb01b40004a
Eyal Z <eyalzh@gmail.com>
2017-10-05 12:37:15 +0300

    fixed lint warnings

--- src/model/initializeModel.ts
+++ src/model/initializeModel.ts
@@ -10,17 +10,13 @@ export default async function initializeModel(model: TaskModel) {
 
     if (!wasTutorialAdded && boards.length === 0) {
 
-        console.log("tutorial flag not found, adding tutorial data...");
-
         const board = await model.addBoard("Tutorial");
         await model.setCurrentBoard(board);
         const columns = await model.getColumns();
 
         if (columns.length === 0) {
-
             const tutorialTemplate = new TutorialTemplate();
             await tutorialTemplate.applyOnModel(model);
-
         }
 
         await model.setFlagOn(FLAGS.TUTORIAL_ADDED);
