d7861adbb101639c0cd405ef1ba30b8aba1bc284
Eyal Z <eyalzh@gmail.com>
2018-03-29 18:43:54 +0300

    upgrade typescript and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
 
     module: {
-        loaders: [
+        rules: [
             // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
             { test: /\.tsx?$/, loaders: ['react-hot-loader/webpack', 'ts-loader'] },
             { test: /\.css$/, loader: "style-loader!css-loader" }
